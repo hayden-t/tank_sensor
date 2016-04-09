fea1af347476131ce1992a72282c21e002215c7a
hayden-t <hayden@httech.com.au>
2016-04-09 21:45:50 +1000

    major rewrite, version 2 sensor and power management

--- tank_sensor/tank_sensor.ino
+++ tank_sensor/tank_sensor.ino
@@ -6,85 +6,95 @@
 int channel = 84;
 RF24 radio(9,10);
 
+#define MIN_PA
+//#define LOW_PA
+//#define HIGH_PA
 
-byte addresses[][6] = {"1Node","2Node"};
+//#define SLEEP_4
+
+const uint64_t pipes[2] = { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };
 
 typedef struct {
   int distance;
   byte voltage;
 }data;
 
-const int SleepTimes = 0;
-//const int SleepTimes = 75;//x8 sec = sleep timer 75 = 10min
+int txTimes = 3;//times to resend same reading
+int txDelay = 1000;//delay between txTimes //100 seems to work
+
+//const int SleepTimes = 1;
+const int SleepTimes = 75;//x8 sec = sleep timer 75 = 10min
 
 const int speedSound = 343;//m/s
-const int numSamples = 1;
-const int delayLoop = 5000;
 const int timeOut = 26000;//445cm (spec 25cm - 4.5m)
 
 const int TRIG_PIN = 2;
 const int ECHO_PIN = 4;
 const int POWR_PIN = 5;
+const int ULTR_PIN = 3;
+const int RF24_PIN = 8;
 
 void setup() {
   
   Serial.begin(9600);
   printf_begin();
-  printf("\n\rwater_sensor\n\n\r");
+  printf("\n\n\rWATER SENSOR START\n\n\r");
   pinMode(ECHO_PIN, INPUT);//echo
   pinMode(TRIG_PIN, OUTPUT);//triger
-  pinMode(POWR_PIN, OUTPUT);//power switch  
- 
- 
-    radio.begin();
-    radio.setChannel(channel);
-    radio.setDataRate(RF24_250KBPS);
+  pinMode(POWR_PIN, OUTPUT);//power switch
+  pinMode(ULTR_PIN, OUTPUT);//sensor switch
+  pinMode(RF24_PIN, OUTPUT);//sensor switch
+  digitalWrite(ULTR_PIN, LOW);//turn off sensor  
+  digitalWrite(RF24_PIN, LOW);//turn off sensor  
 
-    radio.openWritingPipe(addresses[0]);
-    radio.openReadingPipe(1,addresses[1]);
-    
-    radio.printDetails(); 
- 
 }
 
+void loop(){
+   
 
-void loop() {  
- 
-     int distance = getDistance();      
+     digitalWrite(POWR_PIN, LOW);//turn on boost power
+     printf("\n\rPower ON\n\r");
+     delay(500);
+  
+     int distance = getDistance();
      int voltage = readVcc();
      
-     printf("Distance: %d cm, Voltage: %d\n\n\r",distance, voltage);
-
-     radio.powerUp();
-     data payload = { distance, voltage};
+     printf("\n\rDistance: %d cm, Voltage: %d\n\r",distance, voltage);  
      
-     if (radio.write( &payload, sizeof(payload))) printf("Transmit Success.\n\r");
-     else printf("Transmit Failed.\n\r");
-  
-  
+     transmit(distance, voltage);
+     
      printf("Sleeping");
-     radio.powerDown();
-     
+
+     printf("\n\rPower OFF\n\r");
+     delay(100);
+     digitalWrite(POWR_PIN, HIGH);//turn off power
+    
      int i = 0;
      while(i < SleepTimes){
       
         printf(".%d",SleepTimes - i);//debug - turn off ?
         delay(100);
-        LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
+       #ifdef SLEEP_4 
+         LowPower.powerDown(SLEEP_4S, ADC_OFF, BOD_OFF);
+       #else
+         LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
+       #endif
         i++;
        
      }
-     if(!SleepTimes)delay(1000);
+     if(!SleepTimes)delay(5000);//no sleep
      printf("\n\r");
-    
-}
+     
+}//end loop
 
-int getDistance(){  
-  unsigned long duration;
+int getDistance(){
   
+  printf("\n\rReading Distance\n\r");
   
-  digitalWrite(POWR_PIN, LOW);//turn on
-  delay(100);
+  digitalWrite(ULTR_PIN, HIGH);//turn on sensor
+  delay(500);
+  
+  unsigned long duration; 
 
   digitalWrite(TRIG_PIN, HIGH);//trigger start
   delayMicroseconds(100);
@@ -92,10 +102,63 @@ int getDistance(){
   
   duration = pulseIn(ECHO_PIN, HIGH, timeOut);//read result
   
+
+  digitalWrite(ULTR_PIN, LOW);//turn off sensor  
   delay(100);
-  digitalWrite(POWR_PIN, LOW);//turn off
   
   return (duration * speedSound)/20000;//cm
+}
+
+void transmit(int distance, int voltage) {
+  
+   data payload = { distance, voltage};
+  
+  digitalWrite(RF24_PIN, HIGH);//turn on power
+  delay(500);
+  
+   printf("\n\rRadio Start\n\n\r");
+   SPI.begin();
+   radio.begin();
+   
+   radio.setPayloadSize(sizeof(data));
+   
+   radio.setChannel(channel);
+
+    #ifdef MIN_PA 
+      radio.setPALevel(RF24_PA_MIN);//-12dbm
+    #endif     
+    #ifdef LOW_PA 
+      radio.setPALevel(RF24_PA_LOW);//-12dbm
+    #endif  
+    #ifdef HIGH_PA
+      radio.setPALevel(RF24_PA_HIGH);
+    #endif
+   
+   radio.setDataRate(RF24_250KBPS);
+   
+   radio.setRetries(15,15);
+
+   radio.openWritingPipe(pipes[0]);
+   radio.openReadingPipe(1,pipes[1]);   
+   
+   radio.powerUp();
+   
+   radio.flush_tx();
+   
+   radio.printDetails();printf("\n\r");
+   
+      
+     int i = 0;
+     while(i < txTimes){
+       if (radio.write( &payload, sizeof(payload))) printf("Transmit Success.\n\n\r");//??
+       else printf("Transmit Failed.\n\n\r");
+       delay(txDelay);
+       i++;
+     }
+     
+   digitalWrite(RF24_PIN, LOW);//turn off rf
+     
+
 }
 
 int readVcc() {
@@ -112,9 +175,6 @@ int readVcc() {
   result = 1126400L / result; // Back-calculate AVcc in mV 
   
    result /= 100;
-  
-  //VOLTAGE.addValue(int(result));
-  //result = VOLTAGE.getAverage();
-  
+    
   return result;
 }
--- tank_sensor_base/tank_sensor_base.ino
+++ tank_sensor_base/tank_sensor_base.ino
@@ -9,7 +9,7 @@ int channel = 84;
 RF24 radio(9,10);
 
 
-byte addresses[][6] = {"1Node","2Node"};
+const uint64_t pipes[2] = { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };
 
 typedef struct {
   int distance;
@@ -26,9 +26,16 @@ RTC_DS1307 RTC;
 
 const int LCD_BACKLIGHT = 3;
 
-const int SENS_HEIGHT = 240;
+//25cm min
+const int SENS_HEIGHT = 233;//full heigh 33cm
 const int FULL_HEIGHT = 200;
 
+long lastRecieved;
+
+int i, t;
+
+boolean connected = false;
+int timeout = 12;//mins till timeout
 
 void setup() {
   // put your setup code here, to run once:
@@ -38,7 +45,6 @@ void setup() {
     Wire.begin();  
     RTC.begin();
     //RTC.adjust(DateTime(__DATE__, __TIME__).get());//36000 +10 hrs//uncomment and upload once to set clock, comment again and upload after
- 
     
     lcd.InitLCD();    
     
@@ -48,40 +54,79 @@ void setup() {
     printf("\n\rwater_base\n\n\r");
     
     radio.begin();
+    
+    radio.setPayloadSize(sizeof(data));
+    
     radio.setChannel(channel);
+    
+    radio.setPALevel(RF24_PA_LOW);//-12dbm
+    
     radio.setDataRate(RF24_250KBPS);
 
-    radio.openWritingPipe(addresses[1]);
-    radio.openReadingPipe(1,addresses[0]);
+    radio.openWritingPipe(pipes[1]);
+    radio.openReadingPipe(1,pipes[0]);
     
+    radio.powerUp();
     radio.printDetails(); 
     radio.startListening();   
     
-     sprintf(line, "Hello");
+     sprintf(line, "Top");
      lcd.setFont(SmallFont);
-     lcd.print(line, CENTER, 0);
+     lcd.print(line, LEFT, 0);
      
-     sprintf(line, "45");
+     sprintf(line, "2nd");
+     lcd.setFont(SmallFont);
+     lcd.print(line, RIGHT, 0);   
+     
+     sprintf(line, "100");
      lcd.setFont(BigNumbers);
      lcd.print(line, CENTER, 8);
+     lcd.setFont(SmallFont);
+     lcd.print("%", 62, 16);
      
-     sprintf(line, "another line");
+     sprintf(line, "3rd");
      lcd.setFont(SmallFont);
      lcd.print(line, CENTER, 32);
      
+     sprintf(line, "4th");
+     lcd.setFont(SmallFont);
+     lcd.print(line, CENTER, 40);
      
-     delay(3000);
+     
+     delay(1000);
+     //while(1);
      lcd.clrScr();
+     
+     lastRecieved = RTC.now().get();
     
 }
-int i;
+
 void loop() {
-  // put your main code here, to run repeatedly:
+
   
-  DateTime now = RTC.now();
+     DateTime now = RTC.now();
+  
+     sprintf(line, "%d:%02d%s ", (now.hour()>12?now.hour()-12:now.hour()), now.minute(), (now.hour()<12?"AM":"PM"));
+     lcd.setFont(SmallFont);
+     lcd.print(line, LEFT, 0);
+     
+     DateTime timeSince = now.get()-lastRecieved;
+     
+     if(connected && timeSince.get() > timeout*60){connected = false;t++;}
+     
+     
+     sprintf(line, "%d:%02d/%d ", int(timeSince.get()/60), timeSince.second(),t);
+     lcd.setFont(SmallFont);
+     lcd.print(line, LEFT, 40);
+     
+
   
   if ( radio.available() ){
-      
+    
+       i++;
+       lastRecieved = now.get();
+       connected = true;
+       
        data payload;
        radio.read(&payload, sizeof(payload));
        
@@ -92,20 +137,20 @@ void loop() {
          sprintf(line, "%d",percent);
          lcd.setFont(BigNumbers);
          lcd.print(line, CENTER, 8);
+         lcd.setFont(SmallFont);
+         lcd.print("%", 62, 16);
         
-         
-         sprintf(line, "BATT: %d.%dv", int(payload.voltage/10), payload.voltage-(int(payload.voltage/10)*10));
+         sprintf(line, "%d.%dv", int(payload.voltage/10), payload.voltage-(int(payload.voltage/10)*10));
          lcd.setFont(SmallFont);
-         lcd.print(line, CENTER, 0);
+         lcd.print(line, RIGHT, 0);
          
-         sprintf(line, "D: %d cm", payload.distance);
+         sprintf(line, "%dcm", payload.distance);
          lcd.setFont(SmallFont);
-         lcd.print(line, CENTER, 32);
+         lcd.print(line, RIGHT, 40);       
 
-        i++;
-  }
+        
+  } 
+
  
-   sprintf(line, "%02d:%02d:%02d", now.hour(), now.minute(), now.second());
-   lcd.setFont(SmallFont);
-   lcd.print(line, CENTER, 40);
+
 }
